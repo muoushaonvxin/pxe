363dc5ef50d922b09d1228aed0e1156602ce6be2
seliencepuppet <zhangyuzeoffice@163.com>
2018-10-22 10:46:05 +0800

    Update auto_pxe_centos6.8.sh

--- auto_pxe_centos6.8.sh
+++ auto_pxe_centos6.8.sh
@@ -1,88 +1,81 @@
 #!/bin/bash
 #
 
-# 关闭selinux 和 iptables
 function stop_selinux_iptables(){
-	sed -i 's/SELINUX=enforcing/SELINUX=disable/' /etc/selinux/config
-	setenforce 0
-	# systemctl stop firewalld
-	# systemctl disable firewalld
-	service iptables stop	
+        sed -i 's/SELINUX=enforcing/SELINUX=disable/' /etc/selinux/config
+        setenforce 0
+        service iptables stop
 }
 
-# 检测有没有安装dhcp
 function check_dhcp(){
-	yum -y install -q dhcp &> /dev/null
+        yum -y install -q dhcp &> /dev/null
 }
 
-# 检测有没有安装tftp
-function check_tftp(){	
-	yum -y install -q tftp xinetd tftp-server syslinux &> /dev/null
+function check_tftp(){
+        yum -y install -q tftp xinetd tftp-server syslinux &> /dev/null
 }
 
-function check_httpd(){	
-	yum -y install -q httpd &> /dev/null
+function check_httpd(){
+        yum -t install -q httpd &> /dev/null
 }
 
-# 检测dhcp的配置文件
 function check_dhcp_cfg(){
->/etc/dhcp/dhcpd.conf
+> /etc/dhcp/dhcpd.conf
 
-	cat > /etc/dhcp/dhcpd.conf << EOF
+cat > /etc/dhcp/dhcpd.conf << EOF
 allow booting;
 allow bootp;
 
-subnet 192.168.0.0 netmask 255.255.255.0 {
-	range 192.168.0.50 192.168.0.70;
-    option domain-name-servers ns1.internal.example.org;
-	option domain-name "internal.example.org";
-	option routers 192.168.0.1;
-  	option broadcast-address 192.168.0.255;
-    default-lease-time 600;
-	max-lease-time 7200;
- 	next-server 192.168.0.250;
-    filename "pxelinux.0";
+subnet 192.168.99.0 netmask 255.255.255.0 {
+        range 192.168.99.50 192.168.99.70;
+        option domain-name-servers ns1.internal.example.org;
+        option domain-name "internal.example.org";
+        option routers 192.168.99.1;
+        option broadcast-address 192.168.99.255;
+        default-lease-time 600;
+        max-lease-time 7200;
+        next-server 192.168.99.235;
+        filename "pxelinux.0";
 }
 EOF
 
-# systemctl start dhcpd
 /etc/init.d/dhcpd start
 
 RETVAL=$?
 
 if [ $RETVAL -ne 0 ]; then
-	echo "dhcp_cfg have syntax error.."
-	return 1
+        echo "dhcp_cfg have syntax error.."
+        return 1
 fi
 
 }
 
-# 检测tftp的配置文件
 function check_tftp_cfg(){
 sed -i '/disable/s/yes/no/' /etc/xinetd.d/tftp
 cp -r /mnt/cdrom/isolinux/vesamenu.c32 /var/lib/tftpboot/
 cp -r /usr/share/syslinux/pxelinux.0 /var/lib/tftpboot
 mkdir /var/lib/tftpboot/pxelinux.cfg &> /dev/null
 touch /var/lib/tftpboot/pxelinux.cfg/default
->/var/lib/tftpboot/pxelinux.cfg/default
 
-	cat > /var/lib/tftpboot/pxelinux.cfg/default << EOF
+> /var/lib/tftpboot/pxelinux.cfg/default 
+
+cat > /var/lib/tftpboot/pxelinux.cfg/default << EOF
 default vesamenu.c32
-timeout 600 
+timeout 600
 
 display boot.msg
 menu clear
 menu background splash.jpg
 
-menu title Community Enterprise operating system 6.8 
+menu title Community Enterprise operating system 6.8
 label linux
-	menu label ^Install Community Enterprise operating system 6.8 
-	kernel vmlinuz
-	append initrd=initrd.img ks=http://192.168.0.250/ks.cfg
+        menu label ^Install Community Enterprise operating system 6.8
+        kernel vmlinuz
+        append initrd=initrd.img ks=http://192.168.99.235/ks.cfg
 label local
-    menu label Boot from ^local drive
-    menu default
-    localboot 0xffff
+        menu label Boot from ^local drive
+        menu default
+        localboot 0xffff
 EOF
 
 cp /mnt/cdrom/isolinux/boot.msg /var/lib/tftpboot
@@ -93,32 +86,31 @@ cp /mnt/cdrom/isolinux/vmlinuz /var/lib/tftpboot
 service xinetd restart
 }
 
-
 function check_ks_cfg(){
-	cat > /var/www/html/ks.cfg << EOF
+cat > /var/www/html/ks.cfg << EOF
 #platform=x86, AMD64, or Intel EM64T
 #version=DEVEL
 # Firewall configuration
-	firewall --disabled
+                firewall --disabled
 # Install OS instead of upgrade
-	install
+                install
 # Use network installation
-	url --url="http://192.168.0.250/dvd/"
+                url --url="http://192.168.99.235/dvd/"
 # Root password
-	rootpw --plaintext 123456
+                rootpw --plaintext 123456
 # System authorization information
-	auth  --useshadow  --passalgo=sha512
+                auth  --useshadow  --passalgo=sha512
 # Use graphical install
-	graphical
-	firstboot --disable
+                graphical
+                firstboot --disable
 # System keyboard
-	keyboard us
+                keyboard us
 # System language
-	lang en_US
+                lang en_US
 # SELinux configuration
-	selinux --disabled
+                selinux --disabled
 # Installation logging level
-	logging --level=info
+                logging --level=info
 
 # System timezone
 timezone  Asia/Shanghai
@@ -152,40 +144,39 @@ service httpd restart
 
 }
 
-function main_init() { 
-	stop_selinux_iptables
-	check_dhcp	
-	check_tftp
-	check_httpd
-	check_dhcp_cfg
-	check_tftp_cfg
-	check_ks_cfg
+function main_init(){
+        stop_selinux_iptables
+        check_dhcp
+        check_tftp
+        check_httpd
+        check_dhcp_cfg
+        check_tftp_cfg
+        check_ks_cfg
 }
 
-function main_start() {
-	/etc/init.d/dhcpd start
-	/etc/init.d/xinetd start
-	/etc/init.d/httpd start	
+function main_start(){
+        /etc/init.d/dhcpd start
+        /etc/init.d/xinetd start
+        /etc/init.d/httpd start
 }
 
-function main_stop() {
-	/etc/init.d/dhcpd stop
-	/etc/init.d/xinetd stop
-	/etc/init.d/httpd stop	
+function main_stop(){
+        /etc/init.d/dhcpd stop
+        /etc/init.d/xinetd stop
+        /etc/init.d/httpd stop
 }
-
 
 case "$1" in
-	init)
-		main_init
-	;;
-	start)
-		main_start
-	;;
-	stop)
-		main_stop
-	;;
-	*)
-		echo "Usage: `basename $0` { init | start | stop }."
-	;;
+        init)
+                main_init
+        ;;
+        start)
+                main_start
+        ;;
+        stop)
+                main_stop
+        ;;
+        *)
+                echo "Usage: `basename $0` { init | start | stop }."
+        ;;
 esac
